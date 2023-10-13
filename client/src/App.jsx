6742c9f1d4b17ce0cd63528cf5bb4b2e63ba85f9
Jacob Ehrler <jacobehrler@Jacobs-MBP.lan>
2023-10-13 00:01:50 -0400

    JWT auth with Redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,24 +9,30 @@ import Register from "./containers/Register";
 import NotFound from "./components/NotFound";
 import Layout from "./hocs/Layout"
 
+import { Provider } from "react-redux";
+import store from "./store";
+
+
 import './sass/main.scss';
 
 
 const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Routes>
-        <Route exact path="/" Component={Home}/>
-        <Route exact path="/about" Component={About}/>
-        <Route exact path="/contact" Component={Contact}/>
-        <Route exact path="/listings" Component={Listings}/>
-        <Route exact path="/listings/:id" Component={ListingDetail}/>
-        <Route exact path="/login" Component={Login}/>
-        <Route exact path="/register" Component={Register}/>
-        <Route Component={NotFound}/>
-      </Routes>
-    </Layout>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route exact path="/" Component={Home}/>
+          <Route exact path="/about" Component={About}/>
+          <Route exact path="/contact" Component={Contact}/>
+          <Route exact path="/listings" Component={Listings}/>
+          <Route exact path="/listings/:id" Component={ListingDetail}/>
+          <Route exact path="/login" Component={Login}/>
+          <Route exact path="/register" Component={Register}/>
+          <Route Component={NotFound}/>
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  </Provider>
 
 );
 
